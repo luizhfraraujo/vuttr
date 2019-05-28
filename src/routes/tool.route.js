e8602b397ff1748f2904d5b1fc009bb43e614806
Luiz Araújo <luizhfreitas@outlook.com>
2019-05-28 11:23:50 -0300

    Autorização

--- src/routes/tool.route.js
+++ src/routes/tool.route.js
@@ -3,13 +3,14 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/tool.controller');
+const authService = require('../services/auth.service');
 
 router.get('/', controller.get);
 router.get('/:id', controller.put);
 router.get('/tags/:tag', controller.getByTag);
 
-router.post('/', controller.post);
-router.put('/:id', controller.put);
-router.delete('/:id', controller.delete);
+router.post('/', authService.authorize, controller.post);
+router.put('/:id', authService.authorize, controller.put);
+router.delete('/:id', authService.authorize, controller.delete);
 
 module.exports = router;
